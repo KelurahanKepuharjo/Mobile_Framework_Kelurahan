552e1cd75c8759b4ace36b1007f11facb32dcb70
Achmad Fawaid <fwdachmd@gmail.com>
2023-03-13 00:19:26 +0700

    add fontstyle and color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kepuharjo_framework/Shared/Mycolor.dart';
+import 'package:kepuharjo_framework/Shared/Myfont.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,15 +33,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -95,8 +88,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
+              style: MyFont.inter(fontSize: 12, color: black),
             ),
             Text(
               '$_counter',
